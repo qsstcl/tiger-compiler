80d1db48668ed9bb8c78fb3c8b6971e665fa3ce6
qss <qgs7777@163.com>
2024-12-03 13:31:32 +0000

    temp

--- src/tiger/translate/translate.cc
+++ src/tiger/translate/translate.cc
@@ -1,6 +1,7 @@
 #include "tiger/translate/translate.h"
 
 #include <asm-generic/errno-base.h>
+#include <cassert>
 #include <cstddef>
 #include <llvm-14/llvm/IR/Constants.h>
 #include <llvm-14/llvm/IR/DerivedTypes.h>
@@ -73,12 +74,27 @@ void ProgTr::OutputIR(std::string_view filename) {
   llvm::raw_fd_ostream out(llvmfile, ec, llvm::sys::fs::OpenFlags::OF_Text);
   ir_module->print(out, nullptr);
 }
+void DeclareAllocRecordFunction() {
+  llvm::LLVMContext &context = ir_builder->getContext();
+  llvm::Module *module = ir_module;
 
+  // 定义函数类型
+  llvm::Type *int_type = llvm::Type::getInt32Ty(context);
+  llvm::Type *int_ptr_type = llvm::Type::getInt32PtrTy(context);
+  llvm::FunctionType *func_type =
+      llvm::FunctionType::get(int_ptr_type, {int_type}, false);
+
+  // 在模块中声明函数
+  alloc_record = llvm::Function::Create(
+      func_type, llvm::Function::ExternalLinkage, "alloc_record", module);
+}
 void ProgTr::Translate() {
   FillBaseVEnv();
   FillBaseTEnv();
   /* TODO: Put your lab5-part1 code here */
-  absyn_tree_->Translate(venv_.get(), tenv_.get(), main_level_.get(), errormsg_.get());
+  DeclareAllocRecordFunction();
+  absyn_tree_->Translate(venv_.get(), tenv_.get(), main_level_.get(),
+                         errormsg_.get());
 }
 
 } // namespace tr
@@ -94,7 +110,17 @@ tr::ValAndTy *AbsynTree::Translate(env::VEnvPtr venv, env::TEnvPtr tenv,
 
 void TypeDec::Translate(env::VEnvPtr venv, env::TEnvPtr tenv, tr::Level *level,
                         err::ErrorMsg *errormsg) const {
- /* TODO: Put your lab5-part1 code here */
+  /* TODO: Put your lab5-part1 code here */
+  auto type_dec_list = types_->GetList();
+  for (auto type_dec : type_dec_list) {
+    type::Ty *res_ty = type_dec->ty_->Translate(tenv, errormsg);
+    auto debug_ty = static_cast<type::RecordTy *>(res_ty);
+    if (res_ty == nullptr) {
+      errormsg->Error(pos_, "Translate failed");
+      return;
+    }
+    tenv->Enter(type_dec->name_, new type::NameTy(type_dec->name_, res_ty));
+  }
 }
 
 void FunctionDec::Translate(env::VEnvPtr venv, env::TEnvPtr tenv,
@@ -108,7 +134,7 @@ void VarDec::Translate(env::VEnvPtr venv, env::TEnvPtr tenv, tr::Level *level,
   auto init_val_ty = init_->Translate(venv, tenv, level, errormsg);
   if (init_val_ty == NULL) {
     errormsg->Error(pos_, "Initilization failed");
-    return ;
+    return;
   }
   auto init_ty = init_val_ty->ty_;
   tr::Access *access = tr::Access::AllocLocal(level, escape_);
@@ -122,6 +148,23 @@ type::Ty *NameTy::Translate(env::TEnvPtr tenv, err::ErrorMsg *errormsg) const {
 type::Ty *RecordTy::Translate(env::TEnvPtr tenv,
                               err::ErrorMsg *errormsg) const {
   /* TODO: Put your lab5-part1 code here */
+  std::vector<llvm::Type *> llvm_fields;
+  type::FieldList *ty_fileds = new type::FieldList();
+  for (auto field : record_->GetList()) {
+    type::Ty *field_ty = tenv->Look(field->typ_);
+    if (field_ty == nullptr) {
+      errormsg->Error(pos_, "Undefined type");
+      return nullptr;
+    }
+    type::Field *ty_field = new type::Field(field->name_, field_ty);
+    llvm::Type *llvm_ty = field_ty->GetLLVMType();
+    llvm_fields.push_back(llvm_ty);
+    ty_fileds->Append(ty_field);
+  }
+  llvm::StructType *llvm_record_ty =
+      llvm::StructType::create(llvm_fields, "MyStruct");
+
+  return new type::RecordTy(ty_fileds);
 }
 
 type::Ty *ArrayTy::Translate(env::TEnvPtr tenv, err::ErrorMsg *errormsg) const {
@@ -162,7 +205,8 @@ tr::ValAndTy *IntExp::Translate(env::VEnvPtr venv, env::TEnvPtr tenv,
                                 tr::Level *level,
                                 err::ErrorMsg *errormsg) const {
   /* TODO: Put your lab5-part1 code here */
-  llvm::Value *val = llvm::ConstantInt::get(ir_builder->getInt32Ty(), val_, true);
+  llvm::Value *val =
+      llvm::ConstantInt::get(ir_builder->getInt32Ty(), val_, true);
 
   // 2. 返回包含值和类型的 ValAndTy
   return new tr::ValAndTy(val, type::IntTy::Instance());
@@ -196,14 +240,16 @@ tr::ValAndTy *RecordExp::Translate(env::VEnvPtr venv, env::TEnvPtr tenv,
     return new tr::ValAndTy(nullptr, type::VoidTy::Instance());
   }
   type::RecordTy *record_ty = static_cast<type::RecordTy *>(ty);
-  llvm::StructType *llvm_record_ty = llvm::cast<llvm::StructType>(record_ty->GetLLVMType()->getPointerElementType());
-
+  llvm::StructType *llvm_record_ty = llvm::cast<llvm::StructType>(
+      record_ty->GetLLVMType()->getPointerElementType());
+  std::cout << llvm_record_ty->getStructNumElements() * 8 << std::endl;
   llvm::Value *record_ptr = ir_builder->CreateCall(
-    alloc_record,
-    llvm::ConstantInt::get(ir_builder->getInt32Ty(), llvm_record_ty->getStructNumElements()*8)
-  );
+      alloc_record,
+      {llvm::ConstantInt::get(ir_builder->getInt32Ty(),
+                              llvm_record_ty->getStructNumElements() * 8)});
 
-  record_ptr = ir_builder->CreateIntToPtr(record_ptr, llvm_record_ty->getPointerTo());
+  record_ptr =
+      ir_builder->CreateIntToPtr(record_ptr, llvm_record_ty->getPointerTo());
   int index = 0;
   for (auto field : fields_->GetList()) {
     auto field_val_ty = field->exp_->Translate(venv, tenv, level, errormsg);
@@ -211,14 +257,16 @@ tr::ValAndTy *RecordExp::Translate(env::VEnvPtr venv, env::TEnvPtr tenv,
       errormsg->Error(pos_, "Translate failed");
       return nullptr;
     }
+    assert(field_val_ty->ty_ == record_ty->fields_->Get(index)->ty_);
     auto field_val = field_val_ty->val_;
-    llvm::Value *field_ptr = ir_builder->CreateStructGEP(llvm_record_ty, record_ptr, index,field->name_->Name());
+    llvm::Value *field_ptr = ir_builder->CreateStructGEP(
+        llvm_record_ty, record_ptr, index, field->name_->Name());
     ir_builder->CreateStore(field_val, field_ptr);
     index++;
   }
 
   record_ptr = ir_builder->CreatePtrToInt(
-    record_ptr, llvm::Type::getInt64Ty(ir_builder->getContext()));
+      record_ptr, llvm::Type::getInt64Ty(ir_builder->getContext()));
 
   return new tr::ValAndTy(record_ptr, record_ty);
 }
@@ -228,7 +276,7 @@ tr::ValAndTy *SeqExp::Translate(env::VEnvPtr venv, env::TEnvPtr tenv,
                                 err::ErrorMsg *errormsg) const {
   /* TODO: Put your lab5-part1 code here */
   auto exp_list = this->seq_->GetList();
-  llvm::Value * last_val = nullptr;
+  llvm::Value *last_val = nullptr;
   type::Ty *last_ty = nullptr;
   for (auto exp : exp_list) {
     std::cout << "Actual type: " << typeid(*exp).name() << std::endl;
@@ -264,7 +312,6 @@ tr::ValAndTy *WhileExp::Translate(env::VEnvPtr venv, env::TEnvPtr tenv,
   temp::Label *done_label = temp::LabelFactory::NewLabel();
   tr::ValAndTy *test = test_->Translate(venv, tenv, level, errormsg);
   tr::ValAndTy *body = body_->Translate(venv, tenv, level, errormsg);
-
 }
 
 tr::ValAndTy *ForExp::Translate(env::VEnvPtr venv, env::TEnvPtr tenv,
@@ -283,7 +330,7 @@ tr::ValAndTy *LetExp::Translate(env::VEnvPtr venv, env::TEnvPtr tenv,
                                 tr::Level *level,
                                 err::ErrorMsg *errormsg) const {
   /* TODO: Put your lab5-part1 code here */
-  auto decs_list =  this->decs_->GetList();
+  auto decs_list = this->decs_->GetList();
   for (auto dec : decs_list) {
     dec->Translate(venv, tenv, level, errormsg);
   }
